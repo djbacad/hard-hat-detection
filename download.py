e680974b78567b5a7567aeb77f2e930ba0f5ab21
djbacad <djbacad@gmail.com>
2023-05-16 02:11:01 +0800

    Mask API key download.py

--- download.py
+++ download.py
@@ -1,4 +1,4 @@
 from roboflow import Roboflow
-rf = Roboflow(api_key="2hudbTMMi5m0eOeEgmmQ")
+rf = Roboflow(api_key=<YOUR_API_KEY>)
 project = rf.workspace("joseph-nelson").project("hard-hat-workers")
 dataset = project.version(8).download("yolov8")
